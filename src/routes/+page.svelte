fa705b2f13b8222164a919ba5b0a5ac71b4efcb7
Ingo <me@ingowolf.au>
2025-03-02 22:57:01 +1100

    add basic hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<div class="flex h-screen w-screen items-center justify-center"></div>
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	<div>
+		<h1 class="text-4xl">
+			Hi, we are
+			<br />
+			<span>Smasnug Development</span>
+		</h1>
+		<h2 class="text-2xl">and we make stuff</h2>
+	</div>
+</div>
